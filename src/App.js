3629e26991cabb8994a4a3f9b01e2f6cb46a9f3a
skeleta <ss22072000@gmail.com>
2020-11-21 18:47:30 +0530

    Changed UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Map from "./Map";
+import Logo from "./logo.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__main">
+        <Map />
+        <div className="app__potinfo">
+          <h1>Pothole Info</h1>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Map.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+import "./Map.css";
+
+function Map() {
+  const [viewport, setViewport] = React.useState({
+    latitude: 30.91011,
+    longitude: 77.096687,
+    width: "50vw",
+    height: "100vh",
+    zoom: 8,
+  });
+  return (
+    <div className="app__map">
+      <h1>MAP</h1>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken="pk.eyJ1IjoibGF6enp6emFydXMiLCJhIjoiY2tob2YyajlwMDAzcDJyb3M1ajJmYWd3bSJ9.9Y3tZjiKoMD2slDsdLRi_Q"
+        onViewportChange={(viewport) => {
+          setViewport(viewport);
+        }}
+      >
+        <Marker latitude={30.91011} longitude={77.096687}>
+          <div>SKATE</div>
+        </Marker>
+      </ReactMapGL>
+    </div>
+  );
+}
+
+export default Map;
