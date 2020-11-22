c6fd0f83263715bf98fe87d7e84c814edd646e7f
skeleta <ss22072000@gmail.com>
2020-11-22 09:35:58 +0530

    Changed UI

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <div className="app__main">
         <Map />
         <div className="app__potinfo">
-          <h1>Pothole Info</h1>
+          <h1>POTHOLE INFO</h1>
         </div>
       </div>
     </div>
