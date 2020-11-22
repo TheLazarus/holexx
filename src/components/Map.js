264b8eddf7f9b2ed009aa3a943c9ffc45c0d6c97
skeleta <ss22072000@gmail.com>
2020-11-22 13:40:19 +0530

    Reverted back, made a mistake

--- src/components/Map.js
+++ src/components/Map.js
@@ -42,8 +42,8 @@ function Map(props) {
           {potholeMarkers.map((marker) => (
             <Marker
               key={marker.id}
-              latitude={marker.data().latitude}
-              longitude={marker.data().longitude}
+              latitude={marker.latitude}
+              longitude={marker.longitude}
             >
               <img src={Pothole} height="20px" width="20px" alt="Pothole" />
             </Marker>
